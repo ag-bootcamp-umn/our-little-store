ec26283fb395d02a032405e88c3ba61e92e0c6ca
alejandro <alexgeorgegeer@gmail.com>
2024-01-23 12:34:36 -0600

    Created classes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const widget1 = new Product("Widget 2000", 20, 50)
 const widget2 = new Product("Widget 5000", 50, 20)
 const widget3 = new Product("Widget 8000", 100, 10)
 
-const currentStore = new Store("your-store-name", 9, 16, [widget1, widget2, widget3] )
+const currentStore = new Store("my-little-store", 9, 16, [widget1, widget2, widget3] )
 
 // Use inquirer to welcome the use to the store, and show a list of products for then to choose from
 function start(){
@@ -25,4 +25,9 @@ function buyProduct(){
 
 
 
-start()
+// start()
+
+const nathan = new Customer('Nathan');
+console.log(nathan);
+nathan.buyProduct(currentStore, widget1);
+console.log(nathan);
